f1b16221676ae9895951d9ae33c74137930fc51c
Ignatius Yap <ignatiusyap@gmail.com>
2021-11-22 17:24:06 +0800

    Very bare base game created
    Working code so far. Left to include the timer. Before I can implement the killing of the mole

--- project1/project1.js
+++ project1/project1.js
@@ -1,22 +1,53 @@
 "use strict";
 
-const boxes = 9;
+document.querySelector("#submitbutton").addEventListener("click", creategame);
 
+function creategame() {
+  const playername = document.querySelector("#playername").value;
+  if (playername !== "") {
+    document.querySelector("#createplayerdiv").style.display = "none";
+    const displayname = document.createElement("h3");
+    displayname.innerText = `Player name: ${playername}`;
+    document.querySelector("#displayername").append(displayname);
+    createhitbox(boxes);
+    gamestart();
+  } else {
+    alert("Input a name please");
+  }
+}
+
+const boxes = 9;
 function createhitbox(boxes) {
   for (let i = 0; i < boxes; i++) {
     const hitbox = document.createElement("div");
     hitbox.className = "divbuttoncontainer";
-    hitbox.id = `divbutton${boxes}`;
+    hitbox.id = `divbutton${i}`;
     document.querySelector(".hitboxcontainer").append(hitbox);
     const button = document.createElement("button");
     button.className = "buttons";
-    button.id = `button${boxes}`;
+    button.id = `button${i}`;
     hitbox.appendChild(button);
   }
 }
-createhitbox(boxes);
-function randomMole() {
-  const randomIndex = Math.floor(Math.random() * boxes);
-  console.log(randomIndex);
+
+function gamestart() {
+  const gamestartbutton = document.createElement("button");
+  gamestartbutton.id = "gamestart";
+  gamestartbutton.innerText = "Start Game";
+  document.querySelector("#displayername").appendChild(gamestartbutton);
+  document.querySelector("#gamestart").addEventListener("click", gamecycle);
 }
-randomMole();
+
+function timerstart() {
+  function randomMole() {
+    const randomIndex = Math.floor(Math.random() * boxes);
+    const randombutton = `button${randomIndex}`;
+    //console.log(randombutton);
+    console.log(document.querySelector(`#${randombutton}`));
+  }
+  setInterval(randomMole, 3000);
+}
+
+// function timerend() {
+//   clearInterval();
+// }
